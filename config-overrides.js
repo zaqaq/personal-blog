e5865f4f890f2fb00ceedaf2ac94470e15c8b1d0
win.xin <it8099@vedeng.com>
2020-11-20 16:26:56 +0800

    rename

--- config-overrides.js
+++ config-overrides.js
@@ -5,6 +5,7 @@ const {
     addDecoratorsLegacy, //引入装饰器编译方法
     addWebpackAlias,
     overrideDevServer,
+    disableEsLint,
     addWebpackResolve
 } = require("customize-cra"); //引入对应添加或修改 webpack 配置项的方法
 
@@ -26,17 +27,32 @@ const devServerConfig = () => config => {
         }
     };
 };
+
+const addPublicPath = () => config => {
+    if(config.mode === 'production') config.output.publicPath = './'
+    return config;
+}
+
+// const addPublicPath1 = () => config => {
+//     if(config.mode === 'production') config.output.publicPath = './'
+//     return config;
+// }
+
+
 module.exports = {
     webpack: override( //添加或修改通过webpack 配置通过override包裹
+        disableEsLint(),
         addDecoratorsLegacy(),
         addWebpackAlias({
             '@': resolve('src'),
             '@components': resolve('src/components'),
             '@page': resolve('src/page')
         }),
-        addWebpackResolve({
-            extensions: ['.js', '.jsx', '.json']
-        })
+        // addWebpackResolve({
+        //     extensions: ['.js', '.jsx', '.json']
+        // }),
+        addPublicPath(),
+        //addPublicPath1()
     ),
     devServer: overrideDevServer(
         devServerConfig()
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+//__webpack_public_path__ = 'https://cdn.demo.com';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
